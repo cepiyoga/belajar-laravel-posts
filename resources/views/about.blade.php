fb27b44a6b8f036fe6568d24a27d5be289fb4b2c
cepiyoga <cepiyoga@gmail.com>
2024-01-07 13:18:43 +0700

    update 7 Jan 2024 jam 13:18

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>About</title>
-</head>
-<body>
+
+
+@extends('layouts.main')
+
+@section('container')
     
     <h1>Halamab About</h1>
     <h3>Name: {{ $name }}</h3>
     <h3>Email: {{ $email }}</h3>
     <img src="assets/img/{{ $image }}" alt="gambar">
-
-    
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,63 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',['title'=>'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about',['name'=>'Cepi Yoga', 'email'=>'cepiyoga@gmail.com','image'=>'cepi.jpg']);
+    return view('about',['title'=>'About','name'=>'Cepi Yoga', 'email'=>'cepiyoga@gmail.com','image'=>'cepi.jpg']);
 });
+
+
+
 
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = 
+[
+    [
+        'title' => 'Judul Post Pertama',
+        'slug' => 'judul-post-pertama',
+        'author' => 'Sandika Galih',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo corporis quas cum. Fugit sequi aliquam doloremque eveniet neque tempore, vitae amet. Odio libero tempore omnis velit iste laborum adipisci reiciendis aliquam. Quos esse nesciunt rerum iste amet, asperiores quod velit architecto laudantium? Exercitationem aut laborum sit sequi doloribus voluptatum qui laboriosam ut iure. Soluta dicta sit fuga in laboriosam, deleniti magni ipsa, nihil iusto eos tempora commodi repellendus suscipit. Eveniet nemo ipsum dolorem, excepturi error laudantium fugiat odit illum illo perspiciatis quod inventore harum tenetur ex eum? Dicta iure voluptatem reiciendis quaerat excepturi ut veritatis ullam cumque accusantium. Ut, nostrum!'
+    ],
+    [
+        'title' => 'Judul Post Kedua',
+        'slug' => 'judul-post-kedua',
+        'author' => 'Cepi Yoga Asmara',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo corporis quas cum. Fugit sequi aliquam doloremque eveniet neque tempore, vitae amet. Odio libero tempore omnis velit iste laborum adipisci reiciendis aliquam. Quos esse nesciunt rerum iste amet, asperiores quod velit architecto laudantium? Exercitationem aut laborum sit sequi doloribus voluptatum qui laboriosam ut iure. Soluta dicta sit fuga in laboriosam, deleniti magni ipsa, nihil iusto eos tempora commodi repellendus suscipit. Eveniet nemo ipsum dolorem, excepturi error laudantium fugiat odit illum illo perspiciatis quod inventore harum tenetur ex eum? Dicta iure voluptatem reiciendis quaerat excepturi ut veritatis ullam cumque accusantium. Ut, nostrum!'
+    ]
+];
+    return view('posts',['title'=>'Blog', 'posts' => $blog_posts]);
 });
 
+
+//Single Post
+
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_posts = 
+[
+    [
+        'title' => 'Judul Post Pertama',
+        'slug' => 'judul-post-pertama',
+        'author' => 'Sandika Galih',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo corporis quas cum. Fugit sequi aliquam doloremque eveniet neque tempore, vitae amet. Odio libero tempore omnis velit iste laborum adipisci reiciendis aliquam. Quos esse nesciunt rerum iste amet, asperiores quod velit architecto laudantium? Exercitationem aut laborum sit sequi doloribus voluptatum qui laboriosam ut iure. Soluta dicta sit fuga in laboriosam, deleniti magni ipsa, nihil iusto eos tempora commodi repellendus suscipit. Eveniet nemo ipsum dolorem, excepturi error laudantium fugiat odit illum illo perspiciatis quod inventore harum tenetur ex eum? Dicta iure voluptatem reiciendis quaerat excepturi ut veritatis ullam cumque accusantium. Ut, nostrum!'
+    ],
+    [
+        'title' => 'Judul Post Kedua',
+        'slug' => 'judul-post-kedua',
+        'author' => 'Cepi Yoga Asmara',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo corporis quas cum. Fugit sequi aliquam doloremque eveniet neque tempore, vitae amet. Odio libero tempore omnis velit iste laborum adipisci reiciendis aliquam. Quos esse nesciunt rerum iste amet, asperiores quod velit architecto laudantium? Exercitationem aut laborum sit sequi doloribus voluptatum qui laboriosam ut iure. Soluta dicta sit fuga in laboriosam, deleniti magni ipsa, nihil iusto eos tempora commodi repellendus suscipit. Eveniet nemo ipsum dolorem, excepturi error laudantium fugiat odit illum illo perspiciatis quod inventore harum tenetur ex eum? Dicta iure voluptatem reiciendis quaerat excepturi ut veritatis ullam cumque accusantium. Ut, nostrum!'
+    ]
+];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if($post['slug'] == $slug){
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post',['title' => 'Single Post', 'post' => $new_post]);
+});
